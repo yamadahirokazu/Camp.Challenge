bfbf333dafb7e61d374da4e5509d0bbae07a78b9
yamadahirokazu <kurona201@gmail.com>
2016-10-11 17:42:15 +0900

    プログラム基礎２　課題

---
+++ CampChallenge_PHP/002.プログラミング基礎学習２/kiso2-7.php
@@ -0,0 +1,7 @@
+﻿<?php
+ $arr = array();
+
+ $arr = array(1,2,3,4,5,6,7);
+ $arr = array('a','b','c','d','e','f','g');
+
+ $a = 10; $b = 100; $c = 'soeda'; $d = 'hayashi'; $e = -20; $f = 118; $g = 'END'
---
+++ CampChallenge_PHP/002.プログラミング基礎学習２/kiso2.php
@@ -0,0 +1,16 @@
+﻿<?php
+  $num = 3;
+  $message = '';
+  switch($num){
+   case 1;
+    $message = 'one';
+    break;
+   case 2;
+    $message = 'two';
+    break;
+   default;
+    $message = '想定外';
+    break;
+ }
+ echo $message;
+
