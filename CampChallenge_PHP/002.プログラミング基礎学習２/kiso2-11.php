53f6b3d53615136ececb8c02f2f9609577216ba4
yamadahirokazu <kurona201@gmail.com>
2016-10-12 16:54:44 +0900

    10/12　応用課題の素因数分解

---
+++ CampChallenge_PHP/002.プログラミング基礎学習２/kiso2-11.php
@@ -0,0 +1,22 @@
+﻿<?php
+ $num = $_GET['param1'];
+ echo $num."= ";
+
+ if($num <= 0 || is_float($num)){
+  print "正の整数を入力してください";
+ }else{
+   for($i = 2; true; ){
+    if (($num % $i) === 0){
+      print $i."* ";
+      $num = $num / $i;
+    }else{
+       $i++;
+    }
+    if($num <= $i){
+       if($num === 1){ print "1 ";}
+       elseif($num >= 10){ print "その他 ";}
+       elseif($num <= 10){ print $i." ";}
+              break;
+          }
+       }
+    }
